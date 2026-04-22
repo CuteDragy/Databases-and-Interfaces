76fd25ca3adcf129ff28d90d41ccf31a4337a1c6
ZY <hfyzp1@nottingham.edu.my>
2026-04-22 16:02:30 +0800

    UAC

--- UAC.php
+++ UAC.php
@@ -9,7 +9,7 @@ require_once "config.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>User Dashboard</title>
-    <link rel="stylesheet" href="UserAccCreator.css" />
+    <link rel="stylesheet" href="UAC.css" />
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 </head>
 
